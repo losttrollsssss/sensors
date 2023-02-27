0fe123be82b29965baed83706af700572f220cbc
losttrollsssss <115019985+losttrollsssss@users.noreply.github.com>
2023-02-28 00:27:13 +0800

    Update main.ino

--- Matrix keyboard/main.ino
+++ Matrix keyboard/main.ino
@@ -1,13 +1,28 @@
-const int button= 0;
-int state = 0;
+const int buttonPin = 0;
+int buttonState = 0;
+String str;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode (button, INPUT);
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+
+  attachInterrupt(0, swap, RISING);
+}
+
+void swap(){
+  buttonState = analogRead(buttonPin);
+
+  if (buttonState >= 1000){str = "Кнопка 6";}
+  if (buttonState >= 513 && buttonState < 1000){str = "Кнопка 5";}
+  if (buttonState >= 300 && buttonState < 500){str = "Кнопка 4";}
+  if (buttonState >= 250 && buttonState < 300){str = "Кнопка 3";}
+  if (buttonState >= 200 && buttonState < 250){str = "Кнопка 2";}
+  if (buttonState >= 150 && buttonState < 200){str = "Кнопка 1";}
+  Serial.println(str);
 }
 
 void loop() {
-    state = analogRead(button);
-    Serial.println(state);
-    delay(150);
+ buttonState = analogRead(buttonPin);
+ Serial.println(buttonState);
+ delay(50);
 }
